9ae9ca269549c48e983d2cc94ef9001db05e7772
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-12-04 22:24:09 -0500

    ISS Spotter III completed.

--- index.js
+++ index.js
@@ -9,15 +9,28 @@
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-const { fetchCoordsByIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
 
 
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.2643', longitude: '-123.0961' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
 
